38ebbe5e09be1bf02be6cf0d2fde017d6015abbd
gumka <legochev97@yandex.ru>
2022-10-03 13:37:56 +0700

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
+/*
 Console.Write("Введите первое целое число ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе целое число ");
@@ -20,3 +20,38 @@ else
     Console.WriteLine($"Число {number2} больше, чем {number1}");
     }
 }
+*/
+
+/*
+//Задача 4:Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.WriteLine("Введите три разных целых числа для нахождения максимального!");
+
+Console.Write("Введите первое целое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.Read());
+
+if (number1 > number2)
+    {
+        max = number1;
+    }
+else
+    {
+        max = number2;
+    }
+
+if (number3 < max)
+        {
+        Console.Write($"Число {max} самое большое");
+        }
+else
+        {
+        Console.Write($"Число {number3} самое большое");
+        }
+// Не могу понять, почему мне нужно начать на Enter чтобы получить результат. Но она работает.
+*/
