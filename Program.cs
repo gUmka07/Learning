e503b3fb06feea132e1a089c376a3b4e2359b219
gumka <legochev97@yandex.ru>
2022-10-03 14:50:01 +0700

    Четвертая задача

--- Program.cs
+++ Program.cs
@@ -75,3 +75,17 @@ else
 }
 
 */
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.Write("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 0;
+while(current <= number)
+{
+    Console.Write(current++ + " ");
+    current++;
+}
+ 
+*/
