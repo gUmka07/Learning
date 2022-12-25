c2bd626a2139e064a06d734a7b357d1ff5a2c4b2
Dmitry <114214910+gUmka07@users.noreply.github.com>
2022-12-25 12:16:55 +0700

    Add files via upload

---
+++ 009/Program.cs
@@ -0,0 +1,53 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+string[] CreateStringArray1(int n) 
+{  
+    string[] array = new string[n];
+        for (int i = 0; i < n; i++) 
+        {
+            Console.Write("Введите элемент массива №{0}:\n", i + 1);
+            array[i] = Console.ReadLine();          
+        }
+    return array;
+}
+
+string[] CreateStringArray2(string[] array) 
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++) 
+        {
+        if(array[i].Length <= 3) 
+        count++;
+        }
+    string[] newArray = new string[count];
+    for(int i = 0, j = 0; i < array.Length; i++) 
+        {
+        if(array[i].Length <= 3) 
+            {
+            newArray[j] = array[i];
+            j++;
+            }
+        }
+    return newArray;
+}
+
+void PrintStringArray(string[] array) 
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++) 
+    {
+        Console.Write("\"" + array[i] + "\"");
+        if(i < array.Length -1)  
+            Console.Write(", ");
+    }
+    Console.Write("]");
+    Console.Write(" -> "); 
+}
+
+Console.Write("Введите количество элементов в массиве:\n");
+int n = Convert.ToInt32(Console.ReadLine()); 
+string[] array = CreateStringArray1(n);
+PrintStringArray(array);
+string[] myArray = CreateStringArray2(array);
+PrintStringArray(myArray);
+Console.Write("Задание выполнено!");
